572a88a00725052e13eeb026b0b6c7ee29362120
Afaf KELAI <ea_kelai@esi.dz>
2021-10-09 15:51:52 +0100

    first and last commit

---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import {useState,useEffect} from 'react'
+const Exercise = props => (
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date.substring(0,10)}</td>
+    <td>
+      {/* <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a> */}
+    </td>
+  </tr>
+)
+
+// export default class ExercisesList extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.deleteExercise = this.deleteExercise.bind(this)
+
+//     this.state = {exercises: []};
+//   }
+
+//   componentDidMount() {
+//     axios.get('http://localhost:5000/exercises/')
+//       .then(response => {
+//         this.setState({ exercises: response.data })
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       })
+//   }
+
+//   deleteExercise(id) {
+//     axios.delete('http://localhost:5000/exercises/'+id)
+//       .then(response => { console.log(response.data)});
+
+//     this.setState({
+//       exercises: this.state.exercises.filter(el => el._id !== id)
+//     })
+//   }
+
+//   exerciseList() {
+//     return this.state.exercises.map(currentexercise => {
+//       return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h3> Exercises</h3>
+//         <table className="table">
+//           <thead className="thead-light">
+//             <tr>
+//               <th>Username</th>
+//               <th>Description</th>
+//               <th>Duration</th>
+//               <th>Date</th>
+//               <th>Actions</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             { this.exerciseList() }
+//           </tbody>
+//         </table>
+//       </div>
+//     )
+//   }
+// }
+
+export default function ExercisesList (props){
+
+  const [Value, setValue] = useState([])
+  useEffect( ()=>{ 
+    axios.get('http://localhost:5000/exercises/')
+        .then(response => {
+          setValue(response.data)
+          console.log(Value);
+        })
+        .catch((error) => {
+          console.log(error);
+        })},[]
+        )
+     const exerciseList=()=>{
+        // {Value.map(el => <div>{el.description}</div>)}
+              return Value.map(currentexercise => {
+                return <Exercise exercise={currentexercise}  key={currentexercise._id}/>;
+              })
+            }
+
+  return (
+          <div>
+            <h3> Exercises</h3>
+            <table className="table">
+              <thead className="thead-light">
+                <tr>
+                  <th>Username</th>
+                  <th>Description</th>
+                  <th>Duration</th>
+                  <th>Date</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+              
+              { exerciseList() }
+              </tbody>
+            </table>
+            <div>Hello</div>
+            
+          </div>
+        )
+
+}
+
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Exo from "./components/exercises-list.component"
+ReactDOM.render(<App />, document.getElementById('root'));
